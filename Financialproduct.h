32118ae91ff02366aa836a332c2e6b73bf20582f
Jamesbrooks010 <42206478+Jamesbrooks010@users.noreply.github.com>
2018-10-03 17:24:16 +0930

    s

--- Financialproduct.h
+++ Financialproduct.h
@@ -1,8 +1,19 @@
 #ifndef FINANCIALPRODUCT_H 
 #define FINANCIALPRODUCT_H
+#include <string>
+
+class financialproduct 
+{ 
+	std::string name;
+	double principalvalue; 
+	double finalvalue;  
+	int termduration; 
+	int interestratepremium; 
 
 
 
+
+}
 
 
 
