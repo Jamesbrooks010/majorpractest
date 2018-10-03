62c34e5584f25aeca09b9e3e40cca2e3a0e9179c
Jamesbrooks010 <42206478+Jamesbrooks010@users.noreply.github.com>
2018-10-03 17:04:33 +0930

    bank struct

---
+++ Bank.h
@@ -0,0 +1,13 @@
+#ifndef BANK_H
+#define BANK_H 
+
+#include<string> 
+#include "Economy.h"
+
+class Bank : public Economy
+{
+	int bankinterestrate; 
+	//will need to include pointer to an array of customers 
+
+	
+} 
