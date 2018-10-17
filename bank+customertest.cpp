44021e27a2d263256a3fcbc57cc64ffefa2cfc49
Jamesbrooks010 <42206478+Jamesbrooks010@users.noreply.github.com>
2018-10-17 17:30:01 +1030

    cant get this Cin to work want to kms

--- bank+customertest.cpp
+++ bank+customertest.cpp
@@ -2,9 +2,44 @@
 #include "Bank.h"
 #include "Customer.h"
 #include "Deposit.h"
+#include "string"
+#include<iostream> 
+using namespace std; 
+
 int main(void)
 {
-	Economy world;
+
+	string economyinput, customercon, name; 
+	double wage,cash,debt; 
+
+	cout << "please name Economoy: "<<endl; 
+	cin >> economyinput ; 
+	Economy a;
+
+//economy has been created in name a.
+
+	cout<< "would you like to create customers (y/n)?: "<< endl;
+	cin >> customercon; 
+
+		if ( customercon == "y")
+		{
+			cout<< "please enter name: "<< endl; 
+			cin>> name; 
+			cout<< "please enter wage" << endl; 
+			cin>> wage; 
+			cout<< "please enter cash"<<endl; 
+			cin>> cash; 
+			cout<<"please enter debt"<< endl; 
+			cin >> debt; 
+
+		Customer name(name,wage,cash,debt);
+			cout << name << "has been created"<< endl;
+
+		}
+		
+
+
+
 	Customer Vince("Vince", 24, 20,0);
 	Vince.add_new_product("vince's savings deposit","deposit",0, 3000, 2);
 	Vince.add_new_product("Vinces homeloan","Loan", 20000, 0, 20);
@@ -19,5 +54,8 @@ int main(void)
 
 	James.returnfinprodirate(); 
 
+} 
 
-}
+
+
+
